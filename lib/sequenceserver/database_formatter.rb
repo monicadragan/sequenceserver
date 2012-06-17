66ea3495f78de005a3445bdbcfde28d453c2d70d
Anurag Priyam <anurag08priyam@gmail.com>
2012-06-17 16:58:16 +0530

    database_formatter: make the logging interface similar to SequenceServer::App's
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- lib/sequenceserver/database_formatter.rb
+++ lib/sequenceserver/database_formatter.rb
@@ -13,9 +13,6 @@ require 'sequenceserver'
 require 'sequenceserver/helpers.rb'
 require 'sequenceserver/sequencehelpers.rb'
 
-LOG = Logger.new(STDOUT)
-LOG.level = Logger::INFO
-
 class DatabaseFormatter
     include SequenceServer
 	include Helpers
@@ -32,31 +29,39 @@ class DatabaseFormatter
       @db_path = (db_path or @app.database)
     end
 
+    def log
+      settings.log
+    end
+
+    def settings
+      @app
+    end
+
     def format_databases
       unless File.directory?(db_path)
-        LOG.fatal("Database directory #{db_path} not found. See './database_formatter --help' for instructions.")
+        settings.log.fatal("Database directory #{db_path} not found. See './database_formatter --help' for instructions.")
         exit
       end
 
         formatted_dbs = %x|#{@app.binaries['blastdbcmd']} -recursive -list #{db_path} -list_outfmt "%f" 2>&1|.split("\n")
         commands = []
         Find.find(db_path) do |file|
-            LOG.debug("Assessing file #{file}..")
+            settings.log.debug("Assessing file #{file}..")
             if File.directory?(file)
-              LOG.debug("Ignoring file #{file} since it is a directory")
+              settings.log.debug("Ignoring file #{file} since it is a directory")
               next
             end
             if formatted_dbs.include?(file)
-              LOG.debug("Ignoring file #{file} since it is already a blast database")
+              settings.log.debug("Ignoring file #{file} since it is already a blast database")
               next
             end
             if File.binary?(file)
-              LOG.debug("Ignoring file #{file} since it is a binary file, not plaintext as FASTA files are")
+              settings.log.debug("Ignoring file #{file} since it is a binary file, not plaintext as FASTA files are")
               next
             end
 
                 if probably_fasta?(file)
-                    LOG.info("Found #{file}")
+                    settings.log.info("Found #{file}")
                     ## guess whether protein or nucleotide based on first 500 lines
                     first_lines = ''
                     File.open(file, 'r') do |file_stream|
@@ -68,7 +73,7 @@ class DatabaseFormatter
                     begin
                         sequence_type = type_of_sequences(first_lines) # returns :protein or :nucleotide
                     rescue
-                        LOG.warn("Unable to guess sequence type for #{file}. Skipping") 
+                        settings.log.warn("Unable to guess sequence type for #{file}. Skipping")
                     end
                     if [ :protein, :nucleotide ].include?(sequence_type)
                         command = ask_make_db_command(file, sequence_type)
@@ -76,29 +81,29 @@ class DatabaseFormatter
                             commands.push(command)
                         end
                     else 
-                        LOG.warn("Unable to guess sequence type for #{file}. Skipping") 
+                        settings.log.warn("Unable to guess sequence type for #{file}. Skipping")
                     end
                 else
-                  LOG.debug("Ignoring file #{file} since it was not judged to be a FASTA file.")
+                  settings.log.debug("Ignoring file #{file} since it was not judged to be a FASTA file.")
                 end
         end
-        LOG.info("Will now create DBs")
+        settings.log.info("Will now create DBs")
         if commands.empty?
           puts "", "#{db_path} does not contain any unformatted database."
           exit
         end
         commands.each do |command|
-			LOG.info("Will run: " + command.to_s)
+			settings.log.info("Will run: " + command.to_s)
             system(command)
         end
-        LOG.info("Done formatting databases. ")
+        settings.log.info("Done formatting databases. ")
         db_table(db_path)
     end
 
     def db_table(db_path)
-        LOG.info("Summary of formatted blast databases:\n")
+        settings.log.info("Summary of formatted blast databases:\n")
         output = %x|#{@app.binaries['blastdbcmd']} -recursive -list #{db_path} -list_outfmt "%p %f %t" &2>1 |
-        LOG.info(output)
+        settings.log.info(output)
     end
 
     def probably_fasta?(file)
@@ -116,17 +121,17 @@ class DatabaseFormatter
 
     # returns command than needs to be run to make db
     def ask_make_db_command(file, type)
-        LOG.info("FASTA file: #{file}")
-        LOG.info("Fasta type: " + type.to_s)
+        settings.log.info("FASTA file: #{file}")
+        settings.log.info("Fasta type: " + type.to_s)
         
         response = ''
         until response.match(/^[yn]$/i) do
-            LOG.info("Proceed? [y/n]: ")
+            settings.log.info("Proceed? [y/n]: ")
             response = STDIN.gets.chomp
         end
 
         if response.match(/y/i)
-            LOG.info("Enter a database title (or will use '#{File.basename(file)}'")
+            settings.log.info("Enter a database title (or will use '#{File.basename(file)}'")
             title = STDIN.gets.chomp
 			title.gsub!('"', "'")
             title = File.basename(file)  if title.empty?
@@ -136,9 +141,9 @@ class DatabaseFormatter
     end
 
     def make_db_command(file,type, title)
-        LOG.info("Will make #{type.to_s} database from #{file} with #{title}")
+        settings.log.info("Will make #{type.to_s} database from #{file} with #{title}")
         command = %|#{@app.binaries['makeblastdb']} -in #{file} -dbtype #{ type.to_s.slice(0,4)} -title "#{title}" -parse_seqids|
-        LOG.info("Returning: #{command}")
+        settings.log.info("Returning: #{command}")
         return(command)
     end
 end
@@ -182,7 +187,7 @@ BANNER
   end
 
   opts.on('-v', '--verbose', 'Print lots of output') do
-    LOG.level = Logger::DEBUG
+    settings.log.level = Logger::DEBUG
   end
 end.parse!
 
