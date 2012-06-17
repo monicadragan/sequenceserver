de07257597441e69edc8ea24eccf2cea0fc2f90d
Anurag Priyam <anurag08priyam@gmail.com>
2012-06-17 17:19:45 +0530

    database_formatter: bring under SequenceServer namespace
    And some whitespace cleanup.
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- lib/sequenceserver/database_formatter.rb
+++ lib/sequenceserver/database_formatter.rb
@@ -1,7 +1,6 @@
 # copyright yannick . wurm at unil . ch
 # Finds files, reads first char. if its '>', read 500 lines. Guess sequence type, ask user for title to format as blast database.
 
-# TODO: bring it under SequenceServer namespace
 # TODO: move the file to a 'command/' sub-directory (probably makes more sense if we have several subcommands)
 # TODO: needs more love (read refactoring) overall
 
@@ -13,12 +12,13 @@ require 'sequenceserver'
 require 'sequenceserver/helpers.rb'
 require 'sequenceserver/sequencehelpers.rb'
 
-class DatabaseFormatter
+module SequenceServer
+  class DatabaseFormatter
     include SequenceServer
-	include Helpers
+    include Helpers
     include SystemHelpers
     include SequenceHelpers
-    
+
     attr_accessor :db_path
 
     def initialize(db_path = nil)
@@ -43,109 +43,111 @@ class DatabaseFormatter
         exit
       end
 
-        formatted_dbs = %x|#{@app.binaries['blastdbcmd']} -recursive -list #{db_path} -list_outfmt "%f" 2>&1|.split("\n")
-        commands = []
-        Find.find(db_path) do |file|
-            settings.log.debug("Assessing file #{file}..")
-            if File.directory?(file)
-              settings.log.debug("Ignoring file #{file} since it is a directory")
-              next
-            end
-            if formatted_dbs.include?(file)
-              settings.log.debug("Ignoring file #{file} since it is already a blast database")
-              next
-            end
-            if File.binary?(file)
-              settings.log.debug("Ignoring file #{file} since it is a binary file, not plaintext as FASTA files are")
-              next
-            end
+      formatted_dbs = %x|#{@app.binaries['blastdbcmd']} -recursive -list #{db_path} -list_outfmt "%f" 2>&1|.split("\n")
+      commands = []
+      Find.find(db_path) do |file|
+        settings.log.debug("Assessing file #{file}..")
+        if File.directory?(file)
+          settings.log.debug("Ignoring file #{file} since it is a directory")
+          next
+        end
+        if formatted_dbs.include?(file)
+          settings.log.debug("Ignoring file #{file} since it is already a blast database")
+          next
+        end
+        if File.binary?(file)
+          settings.log.debug("Ignoring file #{file} since it is a binary file, not plaintext as FASTA files are")
+          next
+        end
 
-                if probably_fasta?(file)
-                    settings.log.info("Found #{file}")
-                    ## guess whether protein or nucleotide based on first 500 lines
-                    first_lines = ''
-                    File.open(file, 'r') do |file_stream|
-                        file_stream.each do |line|
-                            first_lines += line
-                            break if file_stream.lineno == 500
-                        end
-                    end
-                    begin
-                        sequence_type = type_of_sequences(first_lines) # returns :protein or :nucleotide
-                    rescue
-                        settings.log.warn("Unable to guess sequence type for #{file}. Skipping")
-                    end
-                    if [ :protein, :nucleotide ].include?(sequence_type)
-                        command = ask_make_db_command(file, sequence_type)
-                        unless command.nil?
-                            commands.push(command)
-                        end
-                    else 
-                        settings.log.warn("Unable to guess sequence type for #{file}. Skipping")
-                    end
-                else
-                  settings.log.debug("Ignoring file #{file} since it was not judged to be a FASTA file.")
-                end
+        if probably_fasta?(file)
+          settings.log.info("Found #{file}")
+          ## guess whether protein or nucleotide based on first 500 lines
+          first_lines = ''
+          File.open(file, 'r') do |file_stream|
+            file_stream.each do |line|
+              first_lines += line
+              break if file_stream.lineno == 500
+            end
+          end
+          begin
+            sequence_type = type_of_sequences(first_lines) # returns :protein or :nucleotide
+          rescue
+            settings.log.warn("Unable to guess sequence type for #{file}. Skipping")
+          end
+          if [ :protein, :nucleotide ].include?(sequence_type)
+            command = ask_make_db_command(file, sequence_type)
+            unless command.nil?
+              commands.push(command)
+            end
+          else
+            settings.log.warn("Unable to guess sequence type for #{file}. Skipping")
+          end
+        else
+          settings.log.debug("Ignoring file #{file} since it was not judged to be a FASTA file.")
         end
-        settings.log.info("Will now create DBs")
-        if commands.empty?
-          puts "", "#{db_path} does not contain any unformatted database."
-          exit
-        end
-        commands.each do |command|
-			settings.log.info("Will run: " + command.to_s)
-            system(command)
-        end
-        settings.log.info("Done formatting databases. ")
-        db_table(db_path)
+      end
+      settings.log.info("Will now create DBs")
+      if commands.empty?
+        puts "", "#{db_path} does not contain any unformatted database."
+        exit
+      end
+      commands.each do |command|
+        settings.log.info("Will run: " + command.to_s)
+        system(command)
+      end
+      settings.log.info("Done formatting databases. ")
+      db_table(db_path)
     end
 
     def db_table(db_path)
-        settings.log.info("Summary of formatted blast databases:\n")
-        output = %x|#{@app.binaries['blastdbcmd']} -recursive -list #{db_path} -list_outfmt "%p %f %t" &2>1 |
-        settings.log.info(output)
+      settings.log.info("Summary of formatted blast databases:\n")
+      output = %x|#{@app.binaries['blastdbcmd']} -recursive -list #{db_path} -list_outfmt "%p %f %t" &2>1 |
+      settings.log.info(output)
     end
 
     def probably_fasta?(file)
-		return FALSE if File.zero?(file)
-        File.open(file, 'r') do |file_stream|
-            first_line = file_stream.readline
-            if first_line.slice(0,1) == '>'
-                return TRUE
-            else 
-                return FALSE
-            end
+      return FALSE if File.zero?(file)
+      File.open(file, 'r') do |file_stream|
+        first_line = file_stream.readline
+        if first_line.slice(0,1) == '>'
+          return TRUE
+        else
+          return FALSE
         end
+      end
     end
 
 
     # returns command than needs to be run to make db
     def ask_make_db_command(file, type)
-        settings.log.info("FASTA file: #{file}")
-        settings.log.info("Fasta type: " + type.to_s)
-        
-        response = ''
-        until response.match(/^[yn]$/i) do
-            settings.log.info("Proceed? [y/n]: ")
-            response = STDIN.gets.chomp
-        end
+      settings.log.info("FASTA file: #{file}")
+      settings.log.info("Fasta type: " + type.to_s)
 
-        if response.match(/y/i)
-            settings.log.info("Enter a database title (or will use '#{File.basename(file)}'")
-            title = STDIN.gets.chomp
-			title.gsub!('"', "'")
-            title = File.basename(file)  if title.empty?
-            
-            return make_db_command(file,type,title)
-        end
+      response = ''
+      until response.match(/^[yn]$/i) do
+        settings.log.info("Proceed? [y/n]: ")
+        response = STDIN.gets.chomp
+      end
+
+      if response.match(/y/i)
+        settings.log.info("Enter a database title (or will use '#{File.basename(file)}'")
+        title = STDIN.gets.chomp
+        title.gsub!('"', "'")
+        title = File.basename(file)  if title.empty?
+
+        return make_db_command(file,type,title)
+      end
     end
 
     def make_db_command(file,type, title)
-        settings.log.info("Will make #{type.to_s} database from #{file} with #{title}")
-        command = %|#{@app.binaries['makeblastdb']} -in #{file} -dbtype #{ type.to_s.slice(0,4)} -title "#{title}" -parse_seqids|
-        settings.log.info("Returning: #{command}")
-        return(command)
+      settings.log.info("Will make #{type.to_s} database from #{file} with #{title}")
+      command = %|#{@app.binaries['makeblastdb']} -in #{file} -dbtype #{type.to_s.slice(0,4)} -title "#{title}" -parse_seqids|
+      settings.log.info("Returning: #{command}")
+
+      return(command)
     end
+  end
 end
 
 OptionParser.new do |opts|
@@ -191,5 +193,5 @@ BANNER
   end
 end.parse!
 
-app = DatabaseFormatter.new(ARGV[0])
+app = SequenceServer::DatabaseFormatter.new(ARGV[0])
 app.format_databases
