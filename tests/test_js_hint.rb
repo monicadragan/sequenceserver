7a6e8961b80df1f784d23b9af3a1dd447e71de1d
Anurag Priyam <anurag08priyam@gmail.com>
2012-07-11 01:21:59 +0530

    add tests to ensure all JavaScript files pass jshint
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

---
+++ tests/test_js_hint.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+# Usage:
+#
+# run all test suites
+#
+#     $ tests/run tests/test_js_hint.rb
+#
+# run a particular test suite
+#
+#     $ tests/run tests/test_js_hint.rb public/js/sequenceserver.js
+
+# jshint executable
+jshint = `which jshint`.chomp
+
+# install if jshint not present
+if jshint.empty?
+  system 'npm install jshint'
+  jshint = File.expand_path(File.join('node_modules', 'jshint', 'bin', 'hint'))
+end
+
+# SS root directory
+root = File.dirname(File.dirname(__FILE__))
+
+# jshint specification
+jshintrc = File.join(root, '.jshintrc')
+
+# get list of scripts to run through jshint
+scripts = if ARGV.empty?
+            %w|jquery.index.js sequenceserver.js jquery.scrollspy.js sequenceserver.blast.js|.map {|script| File.join(root, 'public', 'js', script) }
+          else
+            ARGV.map {|path| File.expand_path(path)}
+          end
+
+# run _all_ scripts through jshint; exit with a failure status if any script
+# fails
+success = true
+scripts.each do |script|
+  output = `#{jshint} --config #{jshintrc} #{script}`
+  unless $?.success?
+    puts
+    puts "****************************************"
+    puts "JSHint test failed for '#{script}'."
+    puts
+    puts output
+    puts "****************************************"
+    puts
+    success = false
+  end
+end
+
+exit success
