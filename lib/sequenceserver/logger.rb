dad6bdbe0145487b1cc126e4a96d0b48030e5c6f
Anurag Priyam <anurag08priyam@gmail.com>
2012-09-30 21:31:28 +0530

    Log.
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

---
+++ lib/sequenceserver/logger.rb
@@ -0,0 +1,20 @@
+module SequenceServer
+
+  Log = Logger.new(STDERR)
+
+  # We change Logging format so that it is consistent with Sinatra's
+  Log.formatter = Class.new Logger::Formatter do
+
+    def initialize
+      self.datetime_format = "%Y-%m-%d %H:%M:%S"
+    end
+
+    def format
+      "[%s] %s  %s\n"
+    end
+
+    def call(severity, time, progname, msg)
+      format % [format_datetime(time), severity, msg2str(msg)]
+    end
+  end.new
+end
